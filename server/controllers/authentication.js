60f911ec8f61dfbbb858b2d90ff6a3feffa7e712
Sean Rose <sean1rose@gmail.com>
2017-01-05 02:03:34 -0800

    handle rest of authentication controller aka create and save user record in mongodb

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,4 @@
-// Authentication Controller
+// ***Authentication Controller - Handling the post request of user signup -> either user already exists or save user to DB**
   // this is where any request is gonna be piped to w/ a post request to signup route
 
 // router defines the route user can visit -> upon hitting signup route and posting -> execute Authentication function, which sends back a json response (res.send)
@@ -9,6 +9,7 @@ const User = require('../models/user');
 
 
 // ***GOAL: read in a user if one is passed via signup post request, check to see if user w/ that email already exists, then save the record and respond w/ a success response
+// THIS IS THE SIGNUP ROUTE
 exports.signup = function(req, res, next) {
   // 0. pull out info from request object (which should have email and pw data) of a post request
     // ***to pull out data -> use REQ.BODY (anything contained w/in the body of the post request)*** IMPORTANT
@@ -22,16 +23,37 @@ exports.signup = function(req, res, next) {
   // 1. See if user w/ given email exists
     // need to check thru db records and see if that email exists (if so -> throw error)
     // need ability to check our db records
-    // use mongoose user model (class) w/ method findOne, which checks for email property then calls callback
+    // use mongoose user model ('User' is 'User class') w/ method findOne, which checks for email property then calls callback
     User.findOne( { email: email }, function(err, existingUser) {
       // existingUser will be null if user doesn't exist
-    })
+      // connection to db failed
+      if (err) { return next(err); }
+
+      // 2. if user already exists -> return an error (ERROR HANDLING)
+      if (existingUser) {
+        // set http code on response to 422
+        return res.status(422).send({ error: 'Email is in use' });
+      }
+
+      // 3. if user w/ email does NOT exist -> create and save user record (to create new user -> call 'new' keyword on 'User' class)
+      // create new user (in memory)
+      const user = new User({
+        email: email,
+        password: password
+      });
+      // SAVE the user record to DB
+      user.save(function(err) {
+        if (err) { return next(err); }
+        
+        // 4. respond to request indicating that the user was created (sending back response success true)
+      res.json({ success: true });
+      });
+
+    });
+    // can now test this in postman ^, if successfully save a record in db, automatically get an id assigned
 
 
-  // 2. if user already exists -> return an error
 
-  // 3. if user w/ email does NOT exist -> create and save user record
 
-  // 4. respond to request indicating that the user was created
   
 }
